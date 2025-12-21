41d8eab81f8740801f2a4c8de2f9356682f33090
13beg <94734562+13beg@users.noreply.github.com>
2025-12-21 18:05:17 +0300

    Enhance login with Google process with WebSocket integration

--- src/main/java/cs102groupproject/LoginController.java
+++ src/main/java/cs102groupproject/LoginController.java
@@ -1,5 +1,6 @@
 package cs102groupproject;
 
+import java.util.Map;
 import com.google.api.client.auth.oauth2.Credential;
 import cs102groupproject.SharedObjects.AppEvent;
 import cs102groupproject.Client.GoogleCalendarAPI;
@@ -9,9 +10,20 @@ import cs102groupproject.SharedObjects.AppEvent;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.util.converter.LocalDateStringConverter;
+import cs102groupproject.SharedObjects.ProtocolMessage;
+import cs102groupproject.SharedObjects.ActionType;
+import javafx.scene.control.TextField; //test
 
 public class LoginController {
 
+     //test used with Login1TEST.fxml
+    @FXML
+    private TextField usernameField;
+    //test
+    @FXML
+    private TextField passwordField;
+
+    
     @FXML
     private void switchToSecondary() throws Exception {
 
@@ -20,6 +32,27 @@ public class LoginController {
     @FXML
     private void handleLoginButton() {
         System.out.println("Login button clicked");
+    }
+
+    //test 
+    @FXML
+    private void handleEnterLogin() {
+
+        String username = usernameField.getText();
+        String password = passwordField.getText(); // şimdilik kullanılmayacak
+
+        System.out.println("ENTER pressed → dev login: " + username);
+
+        if (!WebSocketClient.isConnected()) {
+            System.out.println("❌ Not connected to server yet");
+            return;
+        }
+        ProtocolMessage msg = new ProtocolMessage(
+                ActionType.DEV_LOGIN,
+                Map.of("username", username)
+        );
+
+        WebSocketClient.send(msg);
     }
     
     @FXML
@@ -38,19 +71,25 @@ public class LoginController {
                 if (accessToken == null) {
                         throw new RuntimeException("No access token received");
                 }
-    
-                String userId = AuthService.registerWithGoogle(accessToken);
-                GoogleCalendarAPI calendarAPI = new GoogleCalendarAPI(credential);
+                
+                ProtocolMessage msg = new ProtocolMessage(
+                        ActionType.LOGIN_WITH_GOOGLE,
+                        Map.of("accessToken", accessToken)
+                );
+
+                WebSocketClient.send(msg);
+                //String userId = AuthService.registerWithGoogle(accessToken);
+                //GoogleCalendarAPI calendarAPI = new GoogleCalendarAPI(credential);
 
                 // Updates UI on JavaFX App Thread
-                Platform.runLater(() -> {
-                    System.out.println("Login successful, userId=" + userId);
-                    try {
-                        App.setRoot("secondary");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
+                //Platform.runLater(() -> {
+                    //System.out.println("Login successful, userId=" + userId); //CHECK THIS PART userID may be 
+                    //try {
+                        //App.setRoot("secondary");
+                   // } catch (Exception e) {
+                       // e.printStackTrace();
+                    //}
+               // });
     
     
             } catch (Exception e) {

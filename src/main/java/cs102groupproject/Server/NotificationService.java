8f776c01000f3ebcf9afd843be639281d29f9e85
13beg <94734562+13beg@users.noreply.github.com>
2025-12-21 18:17:45 +0300

    Create NotificationService for client notifications
    This class is responsible for sending notifications to clients, enabling asynchronous delivery of server-side updates.

---
+++ src/main/java/cs102groupproject/Server/NotificationService.java
@@ -0,0 +1,8 @@
+package cs102groupproject.Server;
+
+/**
+ * Responsible for sending notifications to clients. Used to deliver server-side updates asynchronously.
+ */
+public class NotificationService {
+    
+}

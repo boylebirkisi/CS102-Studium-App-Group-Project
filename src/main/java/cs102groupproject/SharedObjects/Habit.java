516ac00d99b26ebbda4cac37d6bef8d81358a1fa
boylebirkisi <ali-mersinalimm@hotmail.com>
2025-12-22 12:06:17 +0300

    changed habit class to use string instead of array.

--- src/main/java/cs102groupproject/SharedObjects/Habit.java
+++ src/main/java/cs102groupproject/SharedObjects/Habit.java
@@ -5,14 +5,14 @@ import java.time.LocalDate;
 public class Habit extends TransferObject{
     private String name;
     private LocalDate creationDate;
-    private boolean[] completedArr;
+    private String completionString;
     private String id;
     private String userId;
 
     public Habit(String name, String id, String userId)
     {
         this.name = name;
-        completedArr = new boolean[30];
+        completionString = "000000000000000000000000000000";
         this.id = id;
         this.userId = userId;
         creationDate = LocalDate.now();
@@ -20,23 +20,25 @@ public class Habit extends TransferObject{
 
     public String getName() {return name;}
     public LocalDate getCreationDate() {return creationDate;}
-    public boolean[] getCompletedArr() {return completedArr;}
+    public String getCompletionString() {return completionString;}
     public String getId() {return id;}
     public String getUserId() {return userId;}
 
     public int calculateCompletionCount()
     {
         int completedDays = 0;
-        for (boolean completed : getCompletedArr())
+        for (char completed : getCompletionString().toCharArray())
         {
-            if (completed) completedDays++;
+            if (completed == '1') completedDays++;
         }
         return completedDays;
     }
 
     public void invertCompletedAtIndex(int index)
     {
-        completedArr[index] = !completedArr[index];
+        char[] completionChars = completionString.toCharArray();
+        completionChars[index] = (completionChars[index] == '0') ? '1' : '0';
+        completionString = new String(completionChars);
     }
 
     public void updateHabitAtDate(LocalDate date)

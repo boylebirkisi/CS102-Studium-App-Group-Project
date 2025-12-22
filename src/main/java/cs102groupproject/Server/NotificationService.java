836ed292db36ee920e546e71a9bc8f543ab63f38
Delfin Eryılmaz <deryilmaz06@gmail.com>
2025-12-22 20:36:15 +0300

    Do session informations database and friends request in notification manager.

--- src/main/java/cs102groupproject/Server/NotificationService.java
+++ src/main/java/cs102groupproject/Server/NotificationService.java
@@ -1,8 +1,20 @@
 package cs102groupproject.Server;
-
+import cs102groupproject.SharedObjects.*;
 /**
  * Responsible for sending notifications to clients. Used to deliver server-side updates asynchronously.
  */
 public class NotificationService {
-    
+    public static void sendFriendRequest(int userID, int referenceID, String notification) {
+        Notification notif = new Notification(userID, referenceID, "wants to be your friend!", "New Friend Request", null, Notification.Type.FRIEND_REQUEST);
+        OptionalNotification optNotif = new OptionalNotification(
+            userID,
+            referenceID,
+            notification,
+            "New Friend Request",
+            null,
+            "Accept",
+            "Decline",
+            Notification.Type.FRIEND_REQUEST
+        );
+    }
 }

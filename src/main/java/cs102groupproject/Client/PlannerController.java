b0cc64ea26b7f2aecb2e224a64930db9bfc596b0
boylebirkisi <ali-mersinalimm@hotmail.com>
2025-12-22 12:06:17 +0300

    changed habit class to use string instead of array.

---
+++ src/main/java/cs102groupproject/Client/PlannerController.java
@@ -0,0 +1,11 @@
+package cs102groupproject.Client;
+
+public class PlannerController {
+    SessionManager manager;
+
+    public PlannerController(SessionManager manager) {
+        this.manager = manager;
+    }
+
+    
+}

4a348408da19524aeeedc2a69ffb19b4173b5134
boylebirkisi <ali-mersinalimm@hotmail.com>
2025-12-22 21:34:54 +0300

    temp commit message

--- src/main/java/cs102groupproject/Client/PlannerController.java
+++ src/main/java/cs102groupproject/Client/PlannerController.java
@@ -1,7 +1,30 @@
 package cs102groupproject.Client;
 
 public class PlannerController {
-    SessionManager manager;
+    ClientSession manager;
+    ArrayList<Habit> habits;
+    ArrayList<AppEvent> events;
+    ArrayList<Task> tasks;
+    Habit selectedHabit;
+    LocalDateTime selectedDate;
+    @FXML
+    private Label donePercentTextField;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private ComboBox<Habit> habitTrackerComboBox;
+    @FXML
+    private Label calendarMonthLabel;
+    @FXML
+    private AnchorPane paneDaily;
+    @FXML
+    private FlowPane habitCircleFlowPane;
+    @FXML
+    private TextField addHabitTextField;
+    @FXML
+    private VBox eventsVBox;
+    @FXML
+    private RadioButton dateRadioButton;
 
     public PlannerController(SessionManager manager) {
         this.manager = manager;

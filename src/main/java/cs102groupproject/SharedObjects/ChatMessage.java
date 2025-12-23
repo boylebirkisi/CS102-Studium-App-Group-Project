37e1a772c895bf3de9a0ec044b3b6ccef7d17bb3
boylebirkisi <ali-mersinalimm@hotmail.com>
2025-12-23 19:00:59 +0300

    improved user search function by removing friends and user from the results.

--- src/main/java/cs102groupproject/SharedObjects/ChatMessage.java
+++ src/main/java/cs102groupproject/SharedObjects/ChatMessage.java
@@ -17,6 +17,14 @@ public class ChatMessage {
         this.timestamp = LocalDateTime.now();
     }
 
+    public ChatMessage(int senderID, int receiverID, String message, long timestamp)
+    {
+        this.senderID = senderID;
+        this.receiverID = receiverID;
+        this.message = message;
+        this.timestamp = LocalDateTime.ofEpochSecond(timestamp, 0, java.time.ZoneOffset.UTC);
+    }
+
     public int getSenderID() {return senderID;}
     public int getReceiverID() {return receiverID;}
     public String getMessage() {return message;}

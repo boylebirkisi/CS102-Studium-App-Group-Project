7d777fbacd5b7a2966c2adb1cc58350d7623222b
13beg <94734562+13beg@users.noreply.github.com>
2025-12-23 20:29:15 +0300

    Implement WebSocket listener for group session creation
    Added WebSocket listener for group session creation and refactored payload creation into GroupSession object.

--- src/main/java/cs102groupproject/Client/GroupSessionPlannerPopUpController.java
+++ src/main/java/cs102groupproject/Client/GroupSessionPlannerPopUpController.java
@@ -42,7 +42,16 @@ public class GroupSessionPlannerPopUpController {
         this.ownerController = ownerController;
         populateFriendsList();
     }
-
+    @FXML
+    public void initialize(){
+        WebSocketClient.addListener(
+            ActionType.GROUP_SESSION_CREATED,
+            payload -> {
+                GroupSession session = (GroupSession) payload;
+                System.out.println("✅ Group session created: " + session.getName());
+            }
+        );
+    }
     private void populateFriendsList() {
         ArrayList<User> friendsList = ownerController.getFriendsList();
 
@@ -85,20 +94,12 @@ public class GroupSessionPlannerPopUpController {
 
         User creator = ClientSession.getCurrentUser();
 
-        // Server'a gidecek payload (DTO gibi)
-        Map<String, Object> payload = new HashMap<>();
-        payload.put("creatorId", creator.getId());
-        payload.put("sessionName", sessionName);
-        payload.put("sessionNo", sessionNo);
-        payload.put("sessionLength", sessionLength);
-        payload.put("breakLength", breakLength);
-        payload.put("startDate", startDate.toString());
-        payload.put("isPublic", isPublic);
+        GroupSession groupSessionwithoutId = new GroupSession(creator, new ArrayList<User>(), sessionName, "GROUP_SESSION", sessionNo, sessionLength, breakLength, startDate, isPublic);
 
         // send server with WebSocket
         ProtocolMessage msg = new ProtocolMessage(
                 ActionType.CREATE_GROUP_SESSION,
-                payload
+                groupSessionwithoutId
         );
 
         WebSocketClient.send(msg);

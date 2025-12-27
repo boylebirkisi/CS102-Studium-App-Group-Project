6a6f032863005268e3d8f6db3d4a70afb020d9b4
13beg <94734562+13beg@users.noreply.github.com>
2025-12-27 19:56:54 +0300

    Update SessionManager.java

--- src/main/java/cs102groupproject/Server/SessionManager.java
+++ src/main/java/cs102groupproject/Server/SessionManager.java
@@ -5,20 +5,32 @@ import java.util.Map;
 
 /**
  * Tracks which users are connected to the server.
+ * Acts as a global state for the server package
+ * @author Begüm Göktaş
+ * Date: 27/12/2025
  */
 public class SessionManager {
 
     private final Map<Integer, ClientConnection> onlineUsers =
             new ConcurrentHashMap<>();
 
+    /**
+     * adds the logged-in user to the list of online users
+     * @param int user id
+     * @param ClientConnection 
+     */
     public void login(int userId, ClientConnection connection) {
         onlineUsers.put(userId, connection);
-        System.out.println("🟢 User logged in: " + userId);
+        System.out.println("User logged in: " + userId);
     }
 
+    /**
+     * removes the given connection from the list
+     * @param Client Connection
+     */
     public void logout(ClientConnection connection) {
         onlineUsers.values().remove(connection);
-        System.out.println("🔴 User logged out");
+        System.out.println("User logged out");
     }
 
     public ClientConnection getConnection(int userId) {

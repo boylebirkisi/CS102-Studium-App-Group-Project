091e5667b2de873279d4faa99295daf1d63ba4f1
13beg <94734562+13beg@users.noreply.github.com>
2025-12-27 19:36:00 +0300

    Enhance ChatService with JavaDoc comments

--- src/main/java/cs102groupproject/Server/ChatService.java
+++ src/main/java/cs102groupproject/Server/ChatService.java
@@ -1,7 +1,10 @@
 package cs102groupproject.Server;
 
 import cs102groupproject.SharedObjects.ChatMessage;
-
+/**
+*handles operations related to private messages and manages
+*interactions with the database
+*/
 public class ChatService {
 
     private final DBManager db;
@@ -9,7 +12,11 @@ public class ChatService {
     public ChatService(DBManager db) {
         this.db = db;
     }
-    
+
+    /**
+     * saves the message of the user to database
+       @param Chat message
+     */
     public void saveMessage(ChatMessage cMsg){
         db.insertChatMessage(cMsg.getSenderID(), cMsg.getReceiverID(), cMsg.getMessage(), cMsg.getTimestamp());
     }

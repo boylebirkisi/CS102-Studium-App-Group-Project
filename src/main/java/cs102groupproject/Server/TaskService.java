8741381b599a46f16c7181ae2023e82274ee2a23
13beg <94734562+13beg@users.noreply.github.com>
2025-12-27 20:03:27 +0300

    Update TaskService.java

--- src/main/java/cs102groupproject/Server/TaskService.java
+++ src/main/java/cs102groupproject/Server/TaskService.java
@@ -1,7 +1,12 @@
 package cs102groupproject.Server;
 
 import cs102groupproject.SharedObjects.Task;
-
+/**
+ * responsible for handling server-side task operations
+ * and database interactions.
+ * @author Begüm Göktaş
+ * Date: 27/12/2025
+ */
 public class TaskService {
 
     private final DBManager db;
@@ -9,7 +14,10 @@ public class TaskService {
     public TaskService(DBManager db) {
         this.db = db;
     }
-
+    
+    /**
+     * updates the given task
+     */
     public void updateTask(Task task) {
         boolean success = db.updateTaskStatus(task.getId(), task.getIsCompleted());
         
@@ -20,6 +28,10 @@ public class TaskService {
         }
     }
 
+    /**
+     * saves the given task to the database 
+     * @return saved Task
+     */
     public Task createTask(Task task){
         int id = db.addTask(task.getName(), task.getColor(),task.getImportance(), task.getUserId(), task.getGoogleCaldendarID(), task.getDueDate());
         return new Task(task.getName(), task.getColor(), task.getImportance(), task.getUserId(), id, task.getDueDate(), task.getGoogleCaldendarID(), false);

060bd25f79e721738d73f7bae1e43202165eaddb
13beg <94734562+13beg@users.noreply.github.com>
2025-12-27 20:25:11 +0300

    Update WebSocketServer.java

--- src/main/java/cs102groupproject/Server/WebSocketServer.java
+++ src/main/java/cs102groupproject/Server/WebSocketServer.java
@@ -8,7 +8,7 @@ import jakarta.websocket.server.ServerEndpoint;
 
 /**
  * WebSocket entry point for clients.
- * Authors: Delfin Eryılmaz/ Begüm Göktaş
+ * Authors: Begüm Göktaş
  */
 @ServerEndpoint("/ws")
 public class WebSocketServer {

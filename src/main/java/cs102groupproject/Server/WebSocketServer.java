07444cc8bc886ac2d541ff207a648ee4d7e79598
13beg <94734562+13beg@users.noreply.github.com>
2025-12-27 20:24:55 +0300

    Update WebSocketServer.java

--- src/main/java/cs102groupproject/Server/WebSocketServer.java
+++ src/main/java/cs102groupproject/Server/WebSocketServer.java
@@ -24,7 +24,7 @@ public class WebSocketServer {
 
     @OnMessage
     public void onMessage(String message, Session session) {
-        System.out.println("📩 RAW MESSAGE FROM CLIENT: " + message);
+        System.out.println("RAW MESSAGE FROM CLIENT: " + message);
         ClientConnection connection =
                 (ClientConnection) session
                         .getUserProperties()

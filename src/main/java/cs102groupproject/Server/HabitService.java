22984e3140d18c9eb66216862a669e5140d35db0
13beg <94734562+13beg@users.noreply.github.com>
2025-12-27 19:49:26 +0300

    added javadoc for habitService

--- src/main/java/cs102groupproject/Server/HabitService.java
+++ src/main/java/cs102groupproject/Server/HabitService.java
@@ -1,25 +1,42 @@
 package cs102groupproject.Server;
 
 import cs102groupproject.SharedObjects.Habit;
-
+/**
+ * Performs server-side processing for habit-related operations
+ * and provides database interactions.
+ * Author: Begüm Göktaş
+ * Date: 27/12/2025
+ */
 public class HabitService {
 
     private final DBManager db;
 
-    
     public HabitService(DBManager db){
         this.db = db;
     }
 
+    /**
+     * saves the given habit to the database
+     * @param habit
+     * @return saved habit
+     */
     public Habit createHabit(Habit habit){
         return db.addHabit(habit.getName(), habit.getUserId());
 
     }
-
+    
+    /**
+     * removes the given habit from the database
+     * @param habit
+     */
     public void removeHabit(Habit habit){
         db.removeHabit(habit.getId());
     }
 
+    /**
+     * updates the given habit
+     * @param habit
+     */
     public void updateHabit(Habit habit) {
         db.updateHabitCompletionString(habit.getId(),habit.getCompletionString(), false);
     }
